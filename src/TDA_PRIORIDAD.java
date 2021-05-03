afe483a6006bc83d17690e3e23e8f0d5b1989d59
corre <corre@192.168.1.79>
2021-05-02 21:38:30 -0600

    Updated

---
+++ src/TDA_PRIORIDAD.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author corre
+ */
+public class TDA_PRIORIDAD {
+    char dato;
+    int lvl;
+
+    public TDA_PRIORIDAD() {
+    }
+
+    public TDA_PRIORIDAD(char dato, int lvl) {
+        this.dato = dato;
+        this.lvl = lvl;
+    }
+
+    public char getDato() {
+        return dato;
+    }
+
+    public void setDato(char dato) {
+        this.dato = dato;
+    }
+
+    public int getLvl() {
+        return lvl;
+    }
+
+    public void setLvl(int lvl) {
+        this.lvl = lvl;
+    }
+
+    @Override
+    public String toString(){
+        return "Char: " + dato + ". Lvl de importancia: " + lvl;
+    }
+    
+    
+}
